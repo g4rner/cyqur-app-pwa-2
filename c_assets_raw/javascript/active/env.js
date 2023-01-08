024c735dadd5135e2e4c06d097d33def193b4656
meta.inverse <103123068+g4rner@users.noreply.github.com>
2023-01-08 10:08:29 +0000

    rename manifest.json

--- c_assets_raw/javascript/active/env.js
+++ c_assets_raw/javascript/active/env.js
@@ -1,7 +1,7 @@
 // token api
 const BACKEND_URL_PREFIX = 'https://api.cyqur.io/';
 // set version number
-document.getElementById("version").innerHTML = "v1.23.7.2";
+document.getElementById("version").innerHTML = "v1.23.1.8";
 // check date
 var todayDate = new Date().toISOString().slice(0, 10);
 console.log(todayDate);
